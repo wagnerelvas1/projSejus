700a2f73cccae6f12c82c8f138d1d404be6755fb
ricardopmartins <ricardopmartins.gt.pvh@gmail.com>
2025-12-02 09:20:37 -0400

    Movendo a imagem Home para a pasta de assets e adicionando section para banner em Home

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -3,11 +3,12 @@
 @section('title', 'Home')
 
 @section('styles')
-
+    <link rel="stylesheet" href="{{ asset('assets/css/styleHome.css') }}">
 @endsection
 
 @section('content')
 <body>
+    <section class="banner"> </section>
     <h1>HOME</h1>
     <p> In eget pharetra elit, non placerat leo. Ut malesuada lectus at augue commodo lobortis. Phasellus tempus, lorem porttitor tincidunt euismod, arcu neque aliquet enim, nec consectetur tellus nulla quis sem.</p>
 </body>
