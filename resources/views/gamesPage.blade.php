618edadce97064fd10550b9e53b5b2f9bc47ad9a
Joao <gualberto.silvaj@gmail.com>
2025-12-11 11:40:15 -0400

    Alteração bootstap catalogo

--- resources/views/gamesPage.blade.php
+++ resources/views/gamesPage.blade.php
@@ -7,33 +7,27 @@
 @endsection
 
 @section('content')
-<body>
     <div class="container mt-4">
-        <h1>Catálogo</h1>
+        <h1 class="mb-4">Catálogo</h1>
 
-    </div>
-    <section>
-        <div class="container">
+            <section>
+                <div class="row row-cols-md-3 g-3">
+                    @foreach ($jogos as $jogo)
+                    <div class="col-6 mb-3">
+                        <x-card-catalogo
+                        :id="$jogo->id_jogo"
+                        :title="$jogo->nome_jogo"
+                        :plataform="$jogo->plataforma"
+                        :price="$jogo->final_price"
+                        :original_price="$jogo->valor"
+                        :discount="$jogo->discount"
+                        :img="$jogo->imagem ?? asset('assets/images/defaultGame.jpg')"
+                        />
 
-            <div class="row row-cols-md-3 p-4 ">
-                @foreach ($jogos as $jogo)
-                <div class="col-6 mb-3">
-                    <x-card-catalogo
-                    :id="$jogo->id_jogo"
-                    :title="$jogo->nome_jogo"
-                    :plataform="$jogo->plataforma"
-                    :price="$jogo->final_price"
-                    :original_price="$jogo->valor"
-                    :discount="$jogo->discount"
-                    :img="$jogo->imagem ?? asset('assets/images/defaultGame.jpg')"
-                    />
-
+                    </div>
+                    @endforeach
                 </div>
-                @endforeach
-            </div>
-        </div>
-    </section>
-
-</body>
+            </section>
+</div>
 
 @endsection
